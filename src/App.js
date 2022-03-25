63838db556dfc183bfde1b3aaf8a9bad69676d10
iPadOwner <zenith21c@naver.com>
2022-03-26 01:21:35 +0900

    test

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
       {name==='리액트'?<h1>{name} 안녕</h1>:<h1>React 안녕?</h1>}
       {name=='리액트'&&<h1>안녕 2</h1>}
       <h2>잘 작동하니?</h2>
-      
+      <h2>test</h2>
     </div>
   );
 }
